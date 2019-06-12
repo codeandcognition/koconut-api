d9284d06903a09ec1c92dc34c756996d35a66010
bxie <bxie@uw.edu>
2019-06-12 16:34:42 -0700

    cleaned up code

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-# formerly __init__.py
 from flask import Flask
 from flask_cors import CORS
 
@@ -7,11 +6,3 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 from app import routes
-# import routes
-
-# if __name__ == "__main__":
-#     # Make temp directory
-#     if not os.path.isdir("temp"):
-#         os.mkdir("temp")
-
-#     app.run()
