b9f2b9ffc294493c068807e1168dfce0a1016ecc
bxie <bxie@uw.edu>
2019-06-12 12:48:02 -0700

    removed localhost and port definition. cleanup required

--- run_python.py
+++ run_python.py
@@ -1,12 +1,13 @@
 import os
 from app import app
 
-HOST = os.getenv("HOST", "localhost")
-PORT = os.getenv("PORT", 8080)
+# HOST = os.getenv("HOST", "localhost")
+# PORT = os.getenv("PORT", 8080)
 
 if __name__ == "__main__":
     # Make temp directory
     if not os.path.isdir("temp"):
         os.mkdir("temp")
 
-    app.run(host=HOST, port=PORT)
+    # app.run(host=HOST, port=PORT)
+    app.run()
