130dbe5bbbcdec3e8898b96b3ae9dc67477aff2c
bxie <bxie@uw.edu>
2019-08-29 19:34:18 -0700

    handle differences in string quotes & 0s in floats

--- app/helpers.py
+++ app/helpers.py
@@ -9,3 +9,24 @@ def parse_traceback(traceback):
         return '\n'.join(lines)
     else:
         return traceback
+
+def parse_response(inp):
+    """
+    Replace double quotes with single quotes. 
+    If wrapped in single quotes (or otherwise e.g. wrapped in inconsistent quotes), return unchanged.
+    """
+    # case: user inputs float w/ zero or many trailing 0s
+    if(is_number(inp)):
+        return float(inp)
+
+    # case: user inputs string wrapped in double quotes
+    if inp[0]=='"' and inp[-1]=='"':
+        return "'" + inp[1:-1] + "'"
+    return inp
+
+def is_number(input):
+  try:
+    num = float(input)
+  except ValueError:
+    return False
+  return True
